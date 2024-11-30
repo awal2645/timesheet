e560a79396fa52abe72aeede920e32838b700777
Rakib0101 <rakib.01jan@gmail.com>
2024-11-30 15:43:20 +0600

    uinifomr style applied

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -256,7 +256,7 @@
                         <form action="{{ route('themes.reset') }}" method="POST" class="flex-1 ml-2">
                             @csrf
                             <button type="submit"
-                                class="w-full justify-center text-text-light dark:text-text-dark py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium bg-secondary-300 hover:bg-secondary-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-500 transition-all duration-200">
+                                class="w-full justify-center text-text-light dark:text-text-dark py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium bg-primary-500 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-500 transition-all duration-200">
                                 <svg class="w-5 h-5 mr-2 inline" fill="none" stroke="currentColor"
                                     viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
