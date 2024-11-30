f0ab19a17a7d8602fa1fc95693181bc80300e0cf
Rakib0101 <rakib.01jan@gmail.com>
2024-11-30 13:55:06 +0600

    set some default color

--- database/migrations/2024_11_24_164317_create_themes_table.php
+++ database/migrations/2024_11_24_164317_create_themes_table.php
@@ -10,15 +10,15 @@ return new class extends Migration
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->id();
-            $table->string('primary_color')->default('#8e02d9');
-            $table->string('card_dark')->default('#fca311');
+            $table->string('primary_color')->default('#04a9f5');
+            $table->string('card_dark')->default('#1b232d');
             $table->string('card_light')->default('#fca311');
-            $table->string('sidebar_dark')->default('#0c0f12');
+            $table->string('sidebar_dark')->default('#1d2630');
             $table->string('sidebar_light')->default('#ffffff');
             $table->string('header_dark')->default('#0f1215');
             $table->string('header_light')->default('#f8f9fa');
-            $table->string('body_dark')->default('#0f1215');
-            $table->string('body_light')->default('#f8f9fa');
+            $table->string('body_dark')->default('#131920');
+            $table->string('body_light')->default('#f4f7fa');
             $table->string('font_family')->default('Inter');
             $table->string('text_light')->default('#090606');
             $table->string('text_dark')->default('#cfd3d0');
