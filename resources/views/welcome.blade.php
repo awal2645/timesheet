c2627f6d1ea1e11304b6018d68183285aad0c76e
Rakib0101 <rakib.01jan@gmail.com>
2024-12-11 22:22:21 +0600

    responsive: landing page responsive done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,51 +17,20 @@
         class="py-4 bg-card-dark dark:bg-card-dark inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full">
         <nav class="relative container px-4 xl:px-0 mx-auto w-full md:flex md:items-center md:justify-between"
             aria-label="Global">
-            <div class="flex items-center justify-between">
+            <div class="w-full flex items-center justify-between">
                 <!-- Logo -->
                 <a class="flex-none rounded-md text-2xl text-white inline-block font-semibold focus:outline-none focus:opacity-80"
                     href="/" aria-label="Timesheet">
                     <img src="{{ asset('images/logo-inv.png') }}" alt="Logo" class="w-48 h-auto">
                 </a>
                 <!-- End Logo -->
-
-                <div class="md:hidden">
-                    <button type="button"
-                        class="hs-collapse-toggle size-8 flex justify-center items-center text-sm font-semibold rounded-full bg-transparent text-white disabled:opacity-50 disabled:pointer-events-none"
-                        data-hs-collapse="#navbar-collapse" aria-controls="navbar-collapse"
-                        aria-label="Toggle navigation">
-                        <svg class="hs-collapse-open:hidden flex-shrink-0  shadow-xlsize-4" xmlns="http://www.w3.org/2000/svg"
-                            width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <line x1="3" x2="21" y1="6" y2="6" />
-                            <line x1="3" x2="21" y1="12" y2="12" />
-                            <line x1="3" x2="21" y1="18" y2="18" />
-                        </svg>
-                        <svg class="hs-collapse-open:block hidden flex-shrink-0  shadow-xlsize-4"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M18 6 6 18" />
-                            <path d="m6 6 12 12" />
-                        </svg>
-                    </button>
+                <div>
+                    <a class="group inline-flex items-center gap-x-2 py-2 bg-primary-50 font-medium text-sm text-text-light dark:text-text-dark rounded-md focus:outline-none px-5"
+                        href="{{ route('login') }}" data-aos="fade-up">
+                        {{ __('Login') }}
+                    </a>
                 </div>
             </div>
-
-            <!-- Collapse -->
-            <div id="navbar-collapse"
-                class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block">
-                <div
-                    class="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:items-center md:justify-end md:gap-y-0 md:gap-x-7 md:mt-0 md:ps-7">
-
-                    <div>
-                        <a class="group inline-flex items-center gap-x-2 py-2 bg-primary-50 font-medium text-sm text-text-light dark:text-text-dark rounded-md focus:outline-none px-5"
-                            href="{{ route('login') }}" data-aos="fade-up">
-                            {{ __('Login') }}
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Collapse -->
         </nav>
     </header>
 
