f3ef7887d36f997714478948dd1169eb50c8a5ae
waywise <waywisetechdevelopers@gmail.com>
2025-03-12 15:44:07 +0600

    fix: copyright text alighment issues

--- resources/views/components/app/footer.blade.php
+++ resources/views/components/app/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-header-light dark:bg-header-dark border-b border-slate-200 dark:border-slate-700 z-10 sticky bottom-0">
     <div class="px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16 -mt-px">
+        <div class="flex items-center justify-end h-16 -mt-px">
             <div>
                 <p class="text-sm text-slate-500 dark:text-slate-400">
                     &copy; {{ date('Y') }} Timesheet. All rights reserved.
